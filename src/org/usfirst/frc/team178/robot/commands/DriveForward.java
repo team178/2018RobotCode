5e65ab02effbff72f5471ef79f595a8685d03f79
brandodemko <20demkowiczbr@fpsct.org>
2018-01-20 13:43:19 -0500

    Finished DriveForward Command and made testing command group

--- src/org/usfirst/frc/team178/robot/commands/DriveForward.java
+++ src/org/usfirst/frc/team178/robot/commands/DriveForward.java
@@ -27,20 +27,62 @@ public class DriveForward extends Command {
     protected void initialize() {
     	oi = Robot.oi;
     	drivetrain = Robot.drivetrain;
-    	adjustedSpeed = robotSpeed;
+    	adjustedSpeed = -robotSpeed;
+    	//right motors are negative value because of how they are oriented on the robot
+    	drivetrain.drive(robotSpeed, -robotSpeed);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	//Finds the difference between the speeds of both sides of the robot from the encoders (Ideally want it to be the same so it drives straight)
+    	double diff = drivetrain.getLeftSpeed() - drivetrain.getRightSpeed();
+    	//threshhold to consider what is a sizeable-enough difference in speeds that it needs to be adjusted
+    	if (Math.abs(diff) > 0.01)
+    	{
+    		//adjusts the speed based on a constant * the difference in speeds
+    		adjustedSpeed -= .001 * diff;
+    		//Prints the current speed of both sides of the robot in the console
+    		System.out.println("Right Speed:" + drivetrain.getRightSpeed());
+    		System.out.println("Left Speed:" + drivetrain.getLeftSpeed());
+    		double speedChange = .5 * (distance - drivetrain.getRightSpeed());
+    		if (drivetrain.getLeftDistance() <= .7 * distance )
+    		{
+    			double leftSpeed = robotSpeed * speedChange;
+    			double rightSpeed = adjustedSpeed * speedChange;
+    			double minSpeed = .1;
+    			if (Math.abs(leftSpeed) <= minSpeed && rightSpeed > 0)
+    			{
+    				leftSpeed = minSpeed;
+    			}
+    			else if (Math.abs(rightSpeed) <= minSpeed && rightSpeed < 0)
+    			{
+    				rightSpeed = -minSpeed;
+    			}
+    			drivetrain.drive(leftSpeed, rightSpeed);
+    		}
+    		else
+    		{
+    			drivetrain.leftDrive(robotSpeed);
+    			drivetrain.rightDrive(adjustedSpeed);
+    		}
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+    	if (drivetrain.getLeftSpeed() >= distance)
+    	{
+    		return true;
+    	}
+    	else
+    	{
+    		return false;
+    	}
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	drivetrain.drive(0, 0);
     }
 
     // Called when another command which requires one or more of the same
