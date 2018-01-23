53cbf44a266e6892eb8e339e8ce3d852e5dba111
Unknown <programming@farmingtonrobotics.org>
2018-01-23 17:54:20 -0500

    Edited CubeIntake class

--- src/org/usfirst/frc/team178/robot/subsystems/CubeIntake.java
+++ src/org/usfirst/frc/team178/robot/subsystems/CubeIntake.java
@@ -1,29 +1,30 @@
 package org.usfirst.frc.team178.robot.subsystems;
-
+import org.usfirst.frc.team178.robot.subsystems.*;
 import org.usfirst.frc.team178.robot.RobotMap;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.TalonSRX;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 /**
  *
  */
 public class CubeIntake extends Subsystem {
 
-    public static TalonSRX left1;
-    public static TalonSRX left2;
-    public static TalonSRX right1;
-    public static TalonSRX right2;
+    static TalonSRX left1;
+    static TalonSRX left2;
+    static TalonSRX right1;
+    static TalonSRX right2;
+    DoubleSolenoid doublesolenoid;
     
     public CubeIntake() {
     	left1 = new TalonSRX(RobotMap.INTAKEleft1);
     	left2 = new TalonSRX(RobotMap.INTAKEleft2);
     	right1 = new TalonSRX(RobotMap.INTAKEright1);
-    	right2 = new TalonSRX(RobotMap.INTAKEright2);
-    	
-    
+    	right2 = new TalonSRX(RobotMap.INTAKEright2);    
     }
     
     public void collectCube(double speed) {
@@ -31,6 +32,7 @@ public class CubeIntake extends Subsystem {
     	right2.set(ControlMode.PercentOutput, speed);
     	left1.set(ControlMode.PercentOutput, -speed);
     	left2.set(ControlMode.PercentOutput, -speed);
+    	doublesolenoid.set(DoubleSolenoid.Value.kReverse);
     }
     
     public void shootCube(double speed) {
@@ -38,11 +40,9 @@ public class CubeIntake extends Subsystem {
     	right2.set(ControlMode.PercentOutput, -speed);
     	left1.set(ControlMode.PercentOutput, speed);
     	left2.set(ControlMode.PercentOutput, speed);
-    	
     }
 
     public void initDefaultCommand() {
     	
     }
 }
-
