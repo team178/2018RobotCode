14b285480de4e38d4d93b1cf94f0d8656d3c1b39
Programming <20demkowiczbr@fpsct.org>
2018-02-01 16:51:41 -0500

    Created Autonomous autoCommandGroup are working on it right now

--- src/org/usfirst/frc/team178/robot/autocommandgroups/Autonomous.java
+++ src/org/usfirst/frc/team178/robot/autocommandgroups/Autonomous.java
@@ -2,22 +2,27 @@ package org.usfirst.frc.team178.robot.autocommandgroups;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-import org.usfirst.frc.team178.robot.Robot;
-
-import edu.wpi.first.wpilibj.DriverStation;
 /**
  *
  */
 public class Autonomous extends CommandGroup {
 
     public Autonomous() {
-    	System.out.println("Hotspot?");
-    //	String gameData = DriverStation.getInstance().getGameSpecificMessage();
-    	if (((Robot.temp).getSelected()).equals("Left"))
-    		System.out.println("Left");
-    	else if ((Robot.temp).getSelected().equals("Middle"))
-    		System.out.println("Middle");
-    	else if ((Robot.temp).getSelected().equals("Right"))
-    		System.out.println("Right");
-            }
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
 }
