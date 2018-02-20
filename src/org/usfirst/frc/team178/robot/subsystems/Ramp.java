af52e5fb62f02bdae7455410283ad2c05e843367
Unknown <20fishelro@fpsct.org>
2018-02-20 14:25:33 -0500

    Ultrasonic stuff in collectcube

--- src/org/usfirst/frc/team178/robot/subsystems/Ramp.java
+++ src/org/usfirst/frc/team178/robot/subsystems/Ramp.java
@@ -19,7 +19,6 @@ public class Ramp extends Subsystem {
 	public static Talon right2;
 	public static DoubleSolenoid shootingPiston;
 	public static DoubleSolenoid extendretract;
-	public static AnalogInput ultrasonic;
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
@@ -30,7 +29,6 @@ public class Ramp extends Subsystem {
 		right2 = new Talon(RobotMap.RAMPright2);
 		shootingPiston = new DoubleSolenoid(RobotMap.PCM, RobotMap.shootingPistonOUT, RobotMap.shootingPistonIN);
 		extendretract = new DoubleSolenoid(RobotMap.PCM, RobotMap.LiftShooter, RobotMap.DropShooter);
-		ultrasonic = new AnalogInput(RobotMap.ULTRASONICINPUT);
 	}
 	
 	public void bringCubeIn(double speed) {
@@ -70,9 +68,6 @@ public class Ramp extends Subsystem {
 		extendretract.set(DoubleSolenoid.Value.kReverse);
 	}
 	
-	public double getDistance() {
-		return ultrasonic.getVoltage();
-	}
 	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
